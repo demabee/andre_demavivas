95b1ce374de288b02d9ef1a06860802acfdc4088
Andre Demavivas <andredemavivas@gmail.com>
2023-01-31 05:59:39 +0800

    updated transition animation

--- src/App.js
+++ src/App.js
@@ -188,7 +188,7 @@ function App() {
         <div
           className={`fixed bg-[#6A5A8B] ${open ? 'w-[100%]' : 'w-0'} h-[100vh] text-white flex justify-center items-center z-50 overflow-hidden origin-left duration-500`}
         >
-          <ul className="text-[40px] trackiong-[1px] font-bold text-center">
+          <ul className="text-[40px] tracking-[1px] font-bold text-center">
             <li
               onClick={() => scrollToView(aboutRef)}
               className={`${scrollPos <= 40 ? 'active' : 'hover:textShadow'} duration-500 hover:translate-y-[5px] cursor-pointer`}
